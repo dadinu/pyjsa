d13b3c24664e416ebcbf0b01e9b6d57079973f85
dadinu <dadinu@student.ethz.ch>
2022-12-21 14:09:06 +0100

    cross-system paths

--- src/pyjsa/docs/source/conf.py
+++ src/pyjsa/docs/source/conf.py
@@ -12,8 +12,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..\\..\\..\\'))
-print(sys.path[0])
+from pathlib import Path
+sys.path.insert(0, os.path.abspath(Path(__file__).parents[3]))
 
 
 # -- Project information -----------------------------------------------------
